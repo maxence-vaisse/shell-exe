93e2d5852086be757d6b2bddd94646d1111fc983
maxence-vaisse <145970087+maxence-vaisse@users.noreply.github.com>
2023-09-27 21:17:47 +0200

    Delete Job08 directory

--- Job08/get_logs.sh
+++
@@ -1,5 +0,0 @@
-connection_number=$(grep "LAPTOP-MaxenceMk7" /var/log/auth.log | wc -l)
-
-date=$(date +%d-%m-%Y-%H:%M)
-echo "$connection_number : $date" > "Backup/number_connection-$date.txt"
-tar -czf "Backup/number_connection-$date.tar" "Backup/number_connection-$date.txt"
